02873858326fd04d2d9e6735ae0e57647e717907
Kavindu Praneeth <76823081+kavindu97@users.noreply.github.com>
2023-12-19 13:30:50 +0530

    get without payment cards

--- src/main/java/com/kiu/mainerp/mainerp/repository/AttendanceRepository.java
+++ src/main/java/com/kiu/mainerp/mainerp/repository/AttendanceRepository.java
@@ -107,4 +107,72 @@ public interface AttendanceRepository extends JpaRepository<AttendanceEntity, In
             "WHERE fs.due_date >= '2023-04-01' and fs.due_date < '2023-05-01' and bt.id <> 5\n" +
             "ORDER BY `fs`.`due_date`  DESC;\n",nativeQuery = true)
     Page<Map<String, Object>> gettingIncomeInfoPageination(Pageable pageable);
+    @Query(value = "SELECT obj1.degree, obj1.batch_name, obj1.admission_number, obj1.full_name, obj1.registration_fee_paid, obj1.course_fee_paid,\n" +
+            "obj1.initial_fee_paid, obj1.total_paid_amount, obj1.interest, (obj1.interest + obj1.total_paid_amount) as total_with_fine, obj2.latest_payment_date, obj2.latest_payment_status,  obj2.latest_paid_amount, obj1.ins_due_from, DATEDIFF(CURDATE(), ins_due_from) as no_of_days_outstanding, obj1.currency, \n" +
+            "obj1.tel_mobile1 as mobile_number, obj1.kiu_mail\n" +
+            "FROM (SELECT\n" +
+            "            'TBA' as degree,\n" +
+            "            ba.batch_name,\n" +
+            "            s.range_id as admission_number,           \n" +
+            "            s.full_name, \n" +
+            "            SUM(CASE WHEN (fd.installment_type = 'Registration' AND fs.status <> 'PAID') THEN fs.amount END) AS registration_fee_paid,\n" +
+            "            SUM(CASE WHEN (fd.installment_type = 'Course Fee' AND fs.status <> 'PAID') THEN fs.amount END) AS course_fee_paid,\n" +
+            "            SUM(CASE WHEN (fd.installment_type = 'Initial Fee' AND fs.status <> 'PAID') THEN fs.amount END) AS initial_fee_paid,\n" +
+            "            COALESCE(SUM(CASE WHEN (fd.installment_type = 'Registration' AND fs.status <> 'PAID') THEN fs.amount END), 0) +\n" +
+            "            COALESCE(SUM(CASE WHEN (fd.installment_type = 'Course Fee' AND fs.status <> 'PAID') THEN fs.amount END), 0) +\n" +
+            "            COALESCE(SUM(CASE WHEN (fd.installment_type = 'Initial Fee' AND fs.status <> 'PAID') THEN fs.amount END), 0) as total_paid_amount,\n" +
+            "            fs.late_payment as interest, MIN(fs.due_date) as ins_due_from, fs.currency, s.tel_mobile1, s.kiu_mail\n" +
+            "        FROM\n" +
+            "            finance_student_payment_schedules fs\n" +
+            "        LEFT JOIN finance_student_fee_definitions fd ON fs.fee_definition_id = fd.id\n" +
+            "        LEFT JOIN finance_student_payment_plan_cards fc ON fs.payment_plan_card_id = fc.id\n" +
+            "        LEFT JOIN batch_student bs ON bs.student_id = fc.student_id\n" +
+            "        LEFT JOIN students s ON s.range_id = fc.student_id\n" +
+            "        LEFT JOIN batches ba ON bs.batch_id = ba.batch_id\n" +
+            "        WHERE\n" +
+            "             s.range_id IS NOT NULL\n" +
+            "        GROUP BY\n" +
+            "            s.range_id, ba.batch_name, admission_number, s.full_name\n" +
+            "        HAVING \n" +
+            "        \t MIN(fs.due_date) >= DATE_SUB(CURDATE(), INTERVAL 45 DAY)\n" +
+            "            AND MIN(fs.due_date) <= CURDATE()) obj1\n" +
+            "            LEFT JOIN (\n" +
+            "\n" +
+            "                SELECT\n" +
+            "    fc.student_id,\n" +
+            "    latest_payment.updated_at AS latest_payment_date,\n" +
+            "    fs.status AS latest_payment_status,\n" +
+            "    fs.total_paid AS latest_paid_amount\n" +
+            "FROM\n" +
+            "    finance_student_payment_plan_cards fc\n" +
+            "LEFT JOIN (\n" +
+            "    SELECT\n" +
+            "        fs.payment_plan_card_id,\n" +
+            "        MAX(fs.updated_at) AS updated_at\n" +
+            "    FROM\n" +
+            "        finance_student_payment_schedules fs\n" +
+            "    WHERE\n" +
+            "        fs.status IN ('PAID', 'PARTIAL')\n" +
+            "    GROUP BY\n" +
+            "        fs.payment_plan_card_id \n" +
+            "    HAVING \n" +
+            "    \tMIN(fs.due_date) >= DATE_SUB(CURDATE(), INTERVAL 45 DAY)\n" +
+            "            AND MIN(fs.due_date) <= CURDATE()\n" +
+            ") AS latest_payment ON fc.id = latest_payment.payment_plan_card_id\n" +
+            "LEFT JOIN finance_student_payment_schedules fs ON fc.id = fs.payment_plan_card_id AND latest_payment.updated_at = fs.updated_at\n" +
+            "WHERE\n" +
+            "    fc.student_id IS NOT NULL AND fs.due_date IS NOT NULL\n" +
+            "ORDER BY `latest_payment_date` DESC\n" +
+            "\n" +
+            "                ) obj2 ON obj2.student_id = obj1.admission_number  \n" +
+            "ORDER BY `no_of_days_outstanding` ASC",nativeQuery = true)
+    List<Map<String,Object>> fetchDueReports();
+    @Query(value = "SELECT s.full_name, s.range_id, c.course_name,ba.batch_name,  IF(s.reg_date IS NULL, NULL, YEAR(s.reg_date)) as reg_date " +
+            "FROM students s\n" +
+            "LEFT JOIN finance_student_payment_plan_cards fs ON s.student_id = fs.student_id\n" +
+            "LEFT JOIN batch_student bs ON bs.student_id = s.range_id\n" +
+            "LEFT JOIN courses c ON c.course_id = bs.student_reg_courses_id\n" +
+            "LEFT JOIN batches ba ON bs.batch_id = ba.batch_id\n" +
+            "WHERE s.status = 1 AND fs.id IS NULL;",nativeQuery = true)
+    List<Map<String,Object>>getStudentsWithoutPaymentCards();
 }
